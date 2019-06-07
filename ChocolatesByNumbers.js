b96cbde9d0d9a289989c199bd576baea812d505e
Mark He <kaomask@gmail.com>
2019-06-07 00:33:03 -0400

    improve

--- ChocolatesByNumbers.js
+++ ChocolatesByNumbers.js
@@ -1,11 +1,28 @@
+const timer = require('./timer');
 function solution(N, M) {
-  let count = 0;
-  let i = 0;
-  let eaten = [];
-  while (!eaten[i % N]) {
-    count++;
-    eaten[i % N] = true;
-    i += M;
+  // resolve greatest common divider of N and M
+  if (N === 1) {
+    return 1;
   }
-  return count;
+
+  if (M === 1) {
+    return N;
+  }
+
+  return N / gcd(N, M);
+
 }
+
+function gcd(a, b) {
+  if (a % b === 0) {
+    return b;
+  } else {
+    return gcd(b, a % b);
+  }
+
+}
+
+testCase = [[1000000000, 100], [10,4]];
+timer(() => {
+  testCase.forEach(v=>console.log(solution(...v)));
+});
